e51407f02c02e66131276f323138ba95b4f1353b
Terra Roush <terra.roush@gmail.com>
2020-08-24 21:06:54 -0500

    finsihed js components and functions to display journal entries in the dom. fixes scripts tag in index.html

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -8,17 +8,15 @@ import { useJournalEntries } from "./JournalDataProvider.js"
 import { JournalEntryComponent } from "./JournalEntry.js"
 
 // DOM reference to where all entries will be rendered
-const entryLog = document.querySelector("#entryLog")
 
 export const EntryListComponent = () => {
-    // Use the journal entry data from the data provider component
+    const entryLog = document.querySelector("#entryLog")
+
     const entriesArr = useJournalEntries()
 
+    let entryHTMLRep = "";
     for (const entryObj of entriesArr) {
-        /*
-            Invoke the component that returns an
-            HTML representation of a single entry
-        */debugger
-        entryLog.innerHTML += JournalEntryComponent(entryObj);
+        entryHTMLRep += JournalEntryComponent(entryObj);
     }
+    entryLog.innerHTML += `${entryHTMLRep}`;
 }

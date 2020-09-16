90ace544621d1ac39257cdd067402056c0f48d21
Terra Roush <terra.roush@gmail.com>
2020-09-16 15:22:44 -0500

    other minor adjustments to entryHTML

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -2,8 +2,8 @@ export const JournalEntryHTML = (entryObj) => {
   return `
         <section id="entry--${entryObj.id}" class="journalEntry">
             ${new Date(entryObj.date).toLocaleDateString("en-US")}
-            ${entryObj.concept}:
-            ${entryObj.entry}
+            <h4>${entryObj.concept}:</h3>
+            <p>${entryObj.entry}</p>
         </section>
     `;
 };

b801ef6b2a1bf4808e93ce96bebed0cdf1e7e993
Terra Roush <terra.roush@gmail.com>
2020-10-01 13:29:41 -0500

    set up moodFilter and filterBar

---
+++ scripts/filter/MoodFilter.js
@@ -0,0 +1,16 @@
+export const MoodFilter = () => {
+    return `
+        <fieldset class="fieldset">
+            <legend>Filter Journal Entries by Mood</legend>
+            ${
+                allMoods.map(
+                    (mood) => {
+                        return `<input type="radio" name="moodFilter" value="${ mood.id }"/>
+                        <label for="moodFilter--happy">${ mood.label }</label>
+                        `
+                    }
+                ).join("")
+            }
+        </fieldset>
+        `
+}

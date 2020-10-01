5ee78aba82854b4d1510ee24087f0b0ba7da7479
Terra Roush <terra.roush@gmail.com>
2020-10-01 13:34:45 -0500

    filter class div in index

--- scripts/filter/FilterBar.js
+++ scripts/filter/FilterBar.js
@@ -1,9 +1,5 @@
 import { MoodFilter } from "./MoodFilter.js"
 
-/*
- You need to make a new HTML element with a class of
- `filters` in index.html
-*/
 const contentTarget = document.querySelector(".filters")
 
 const FilterBar = () => {
